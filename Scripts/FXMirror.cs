a830744f80d8504e9f4a1625093606dc2f85feef
Next0r <mchkafka@gmail.com>
2020-05-01 22:06:51 +0200

    init

---
+++ Scripts/FXMirror.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXMirror : MonoBehaviour
+{
+    public Shader effectShader;
+    private Material material;
+    public Camera mirrorCamera;
+    private Texture2D mirrorTexture;
+    public Texture2D mirrorFrameTexture;
+    public Texture2D mirrorFactorTexture;
+
+    private Rect camRect;
+
+    [Range(0.0f, 1.0f)]
+    public float offsetX = 0.55f;
+    [Range(0.0f, 1.0f)]
+    public float offsetY = 0.1f;
+    [Range(0.1f, 1.0f)]
+    public float scaleX = 0.4f;
+    [Range(0.1f, 1.0f)]
+    public float scaleY = 0.4f;
+
+    [Range(0, 1.0f)]
+    public float frameOffsetX = 0.3f;
+    [Range(0, 1.0f)]
+    public float frameOffsetY = 0.7f;
+    [Range(0.1f, 1.0f)]
+    public float frameScaleX = 0.4f;
+    [Range(0.1f, 1.0f)]
+    public float frameScaleY = 0.3f;
+
+    [Range(0.0f, 1.0f)]
+    public float alpha = 0.75f;
+
+    public Color reflectionColor = new Color(1f, 1f, 0.8f, 1f);
+
+    private void Awake() {
+        material = new Material(effectShader); 
+    }
+
+    private void Start()
+    {
+        mirrorTexture = new Texture2D(mirrorCamera.pixelWidth, mirrorCamera.pixelHeight);
+    }
+
+    private void Update()
+    {
+        if (mirrorTexture.width != mirrorCamera.pixelWidth || mirrorTexture.height != mirrorCamera.pixelHeight)
+        {
+            mirrorTexture = new Texture2D(mirrorCamera.pixelWidth, mirrorCamera.pixelHeight);
+        }
+    }
+    private void OnPreRender()
+    {
+        var currentRT = RenderTexture.active;
+        RenderTexture.active = mirrorCamera.targetTexture;
+
+        mirrorCamera.Render();
+        mirrorTexture.ReadPixels(new Rect(0, 0, mirrorCamera.pixelWidth, mirrorCamera.pixelHeight), 0, 0);
+        mirrorTexture.Apply();
+
+        RenderTexture.active = currentRT;
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest) {
+        material.SetFloat("_offsetX", offsetX * 1 / scaleX);
+        material.SetFloat("_offsetY", offsetY * 1 / scaleY);
+        material.SetFloat("_scaleX", 1 / scaleX);
+        material.SetFloat("_scaleY", 1 / scaleY);
+
+        material.SetFloat("_frameOffsetX", -frameOffsetX * 1 / frameScaleX);
+        material.SetFloat("_frameOffsetY", -frameOffsetY * 1 / frameScaleY);
+        material.SetFloat("_frameScaleX", 1 / frameScaleX);
+        material.SetFloat("_frameScaleY", 1 / frameScaleY);
+
+        material.SetFloat("_alpha", alpha);
+        material.SetVector("_reflectionColor", reflectionColor);
+
+        material.SetTexture("_MirrorTex", mirrorTexture);
+        material.SetTexture("_FrameTex", mirrorFrameTexture);
+        material.SetTexture("_FactorTex", mirrorFactorTexture);
+        Graphics.Blit(src, dest, material);
+    }
+
+    
+
+}
---
+++ Scripts/Mirror.shader
@@ -0,0 +1,81 @@
+﻿Shader "Hidden/Mirror"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            sampler2D _MainTex;
+			sampler2D _MirrorTex;
+			sampler2D _FactorTex;
+			sampler2D _FrameTex;
+
+			float _offsetX;
+			float _offsetY;
+			float _scaleX;
+			float _scaleY;
+
+			float _frameOffsetX;
+			float _frameOffsetY;
+			float _frameScaleX;
+			float _frameScaleY;
+
+			float _alpha;
+
+			float3 _reflectionColor;
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				//fixed4 col = tex2D(_MainTex, float2(tan(2 * i.uv.x + 2.15)/5 + 0.5, tan(2 * i.uv.y + 2.15) / 5 + 0.5));
+				fixed4 col = tex2D(_MainTex, i.uv);
+
+				float2 mirrorMapping = float2(-(i.uv.x * _scaleX + _offsetX), i.uv.y * _scaleY + _offsetY);
+                //fixed4 mirror = tex2D(_MirrorTex, mirrorMapping);
+
+				fixed4 mirror = tex2D(_MirrorTex, float2(tan(2 * mirrorMapping.x + 2.15) / 5 + 0.5, mirrorMapping.y));
+
+				float2 frameMapping = float2(i.uv.x * _frameScaleX + _frameOffsetX, i.uv.y * _frameScaleY + _frameOffsetY);
+				fixed4 frame = tex2D(_FrameTex, frameMapping);
+				fixed4 factor = tex2D(_FactorTex, frameMapping);
+				
+				col.rgb = (mirror.rgb * factor.rgb * _alpha * _reflectionColor.rgb) + (col.rgb * (1 - factor.rgb))  + (col.rgb * factor.rgb * (1 - _alpha));
+				col.rgb = col.rgb * (1 - frame.a) + (frame.rgb * frame.a * _alpha) + (col.rgb * frame.a * (1 - _alpha));
+
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
